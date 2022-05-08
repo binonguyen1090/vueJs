906a6109d3b3f5f3f2c15a8ba48e68ae0f3cafa1
Bino <binonguyen1090@gmail.com>
2022-05-08 13:32:32 -0500

    first Vue JS

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,12 +1,38 @@
-let buttonEl =document.querySelector('button');
-let inputEl =document.querySelector('input');
-let listEl =document.querySelector('ul');
-function addGoal() {
-    let enteredValue=inputEl.value;
-    let listItemEl=document.createElement ('li');
-    listItemEl.textContent=enteredValue;
-    listEl.appendChild(listItemEl);
-    inputEl.value='';
-}
+
+Vue.createApp({
+    data(){
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue)
+        }
+    }
+}).mount('#app');
+
+
+
+
+
+
+
+
+
+
+
+
+// let buttonEl =document.querySelector('button');
+// let inputEl =document.querySelector('input');
+// let listEl =document.querySelector('ul');
+// function addGoal() {
+//     let enteredValue=inputEl.value;
+//     let listItemEl=document.createElement ('li');
+//     listItemEl.textContent=enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value='';
+// }
  
-buttonEl.addEventListener('click', addGoal);
+// buttonEl.addEventListener('click', addGoal);
