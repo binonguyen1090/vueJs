487c30d91a78b6e89447355e634e034d66995ea4
Bino <binonguyen1090@gmail.com>
2022-05-08 22:35:44 -0500

    Vue data and method

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,38 +1,21 @@
-
-Vue.createApp({
+const app = Vue.createApp({
     data(){
         return {
-            goals: [],
-            enteredValue: ''
+            courseGoalA: '<h3>Finish Good</h3>',
+            courseGoalB: 'Finish Bad ',
+            vueLink: 'https://www.google.com/',
         }
     },
     methods: {
-        addGoal(){
-            this.goals.push(this.enteredValue)
+        outputGoal(){
+            const randomNumber = Math.random();
+            if(randomNumber > 0.5){
+                return this.courseGoalA 
+            }else{
+                return this.courseGoalB 
+            }
         }
     }
-}).mount('#app');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-// let buttonEl =document.querySelector('button');
-// let inputEl =document.querySelector('input');
-// let listEl =document.querySelector('ul');
-// function addGoal() {
-//     let enteredValue=inputEl.value;
-//     let listItemEl=document.createElement ('li');
-//     listItemEl.textContent=enteredValue;
-//     listEl.appendChild(listItemEl);
-//     inputEl.value='';
-// }
- 
-// buttonEl.addEventListener('click', addGoal);
+app.mount('#user-goal')
